31bb04d68f0bb65a186585e764490c4241037197
TheCalypso <bastien.dorangeon.penichot@gmail.com>
2024-03-12 11:37:25 +0100

    feat: buzzer with bp

--- main.cpp
+++ main.cpp
@@ -8,41 +8,36 @@
 #include <cstdint>
 #include <chrono>
 
-#define WAIT_TIME_MS 500
+/* CONST */
+#define WAIT_TIME_500_MS 500
+#define WAIT_TIME_750_MS 750
+
 /* OUT */
-
-// DigitalOut led1(LED1);
-// DigitalOut cmd_frein(A2);
-
-DigitalOut cmd_phare(D0);
+PwmOut buzzer(A1);
 
 
 /* IN */
-// DigitalIn bp_frein(D11, PullUp);
-AnalogIn photor(A0);
+DigitalIn bp_buzzer(D1, PullUp);
 
 /* Var */
-bool etat_bp_frein;
-uint16_t luminosite;
-char txt[30];
+
 
 int main()
 {
     printf("Running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
     // cmd_frein.write(0); // place le niveau bas =  0V sur la broche A2
+    
+    buzzer.write(0.5);
 
     while (true)
     {
-        luminosite = photor.read_u16();
-        sprintf(txt, "valeur_lum: %d\n", luminosite);
-        printf("%s", txt);
-
-        if(luminosite < 30000){
-            cmd_phare.write(1);
-        } else {
-            cmd_phare.write(0);
+        if (bp_buzzer == 0){
+            buzzer.period_ms(1);
         }
-
-        thread_sleep_for(WAIT_TIME_MS);
+        else {
+            buzzer.period_ms(50000);
+        }
+        thread_sleep_for(2);
+        
     }
 }
