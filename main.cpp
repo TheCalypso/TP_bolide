1b3e2ff39f86e2bb81ab41a12e88772408724a47
TheCalypso <bastien.dorangeon.penichot@gmail.com>
2024-03-12 10:54:40 +0100

    feat: head light with luminosity

--- main.cpp
+++ main.cpp
@@ -4,31 +4,45 @@
  */
 
 #include "mbed.h"
+#include <cstdio>
+#include <cstdint>
+#include <chrono>
 
 #define WAIT_TIME_MS 500
+/* OUT */
+
 // DigitalOut led1(LED1);
-DigitalOut cmd_frein(A2);
-DigitalIn bp_frein(D11, PullUp);
+// DigitalOut cmd_frein(A2);
+
+DigitalOut cmd_phare(D0);
+
+
+/* IN */
+// DigitalIn bp_frein(D11, PullUp);
+AnalogIn photor(A0);
+
+/* Var */
 bool etat_bp_frein;
+uint16_t luminosite;
+char txt[30];
 
 int main()
 {
     printf("Running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
-    cmd_frein.write(0); // place le niveau bas =  0V sur la broche A2
+    // cmd_frein.write(0); // place le niveau bas =  0V sur la broche A2
 
     while (true)
     {
-        // led1 = !led1;
-        // thread_sleep_for(WAIT_TIME_MS);
-        etat_bp_frein = bp_frein.read();
+        luminosite = photor.read_u16();
+        sprintf(txt, "valeur_lum: %d\n", luminosite);
+        printf("%s", txt);
 
-        if(etat_bp_frein == 0){
-            cmd_frein.write(1);
-            thread_sleep_for(WAIT_TIME_MS);
+        if(luminosite < 30000){
+            cmd_phare.write(1);
+        } else {
+            cmd_phare.write(0);
         }
-        else {
-            cmd_frein.write(0);
-        }
-        thread_sleep_for(2);
+
+        thread_sleep_for(WAIT_TIME_MS);
     }
 }
