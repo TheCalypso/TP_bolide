ae3293b7366ea105014dd34145a58e92619e863c
TheCalypso <bastien.dorangeon.penichot@gmail.com>
2024-03-12 09:30:18 +0100

    feat: exercice light when bp ON

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@
 
 #include "mbed.h"
 
-#define WAIT_TIME_MS 500 
+#define WAIT_TIME_MS 500
 // DigitalOut led1(LED1);
 DigitalOut cmd_frein(A2);
+DigitalIn bp_frein(D11, PullUp);
+bool etat_bp_frein;
 
 int main()
 {
@@ -18,9 +20,15 @@ int main()
     {
         // led1 = !led1;
         // thread_sleep_for(WAIT_TIME_MS);
-        cmd_frein.write(0);
-        thread_sleep_for(WAIT_TIME_MS);
-        cmd_frein.write(1);
-        thread_sleep_for(WAIT_TIME_MS);
+        etat_bp_frein = bp_frein.read();
+
+        if(etat_bp_frein == 0){
+            cmd_frein.write(1);
+            thread_sleep_for(WAIT_TIME_MS);
+        }
+        else {
+            cmd_frein.write(0);
+        }
+        thread_sleep_for(2);
     }
 }
